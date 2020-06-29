339a5e8b2019b931526ad6c2653e333fb6ab2e9d
Sebas <sebas0163@hotmail.com>
2020-06-28 22:11:01 -0600

    proyecto unity

---
+++ JuegoUnity/Assets/Script/MovJugador.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovJugador : MonoBehaviour
+{
+    public float Vel;
+    private Rigidbody2D rigidbody;
+    private Vector3 cambio;
+    private Animator animador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animador = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cambio = Vector3.zero;
+        cambio.x = Input.GetAxisRaw("Horizontal");
+        cambio.y = Input.GetAxisRaw("Vertical");
+        animMov();
+        
+    }
+    void animMov(){
+        if(cambio != Vector3.zero){
+            Movimiento();
+            animador.SetFloat("moverX",cambio.x);
+            animador.SetFloat("moverY",cambio.y);
+            animador.SetBool("camina",true);
+        }else{
+            animador.SetBool("camina",false);
+        }
+    }
+
+    void Movimiento(){
+        rigidbody.MovePosition(
+            transform.position + cambio * Vel * Time.deltaTime
+        );
+    }
+}
