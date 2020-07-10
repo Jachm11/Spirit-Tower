3b4ac81cacd81b0d51db8e3b7b2484fdd04aa6a7
Natalia GB <natigonzalezbermudez@gmail.com>
2020-07-09 18:45:52 -0600

    Arreglos de logica

--- Server/ServerVS/Espectro.cpp
+++ Server/ServerVS/Espectro.cpp
@@ -1,19 +1,13 @@
 #include "Espectro.h"
 
-Espectro::Espectro()
+Espectro::Espectro() {}
+Espectro::Espectro(string defRoute)
 {
-	speed;
-	range;
-	life;
-	attacking=false;
-	backtracking=false;
-	playerPos;
-	defaultRoute;
-	attackRoute;
-	backtrackRoute;
-	nextPos;
+	paralized = false;
+	attacking = false;
+	backtracking = false;
 	posIndex = 0;
-	defaultRouteVec();
+	//defaultRouteVec(defRoute);
 	
 	//Cosas que metio Jose :3
 
@@ -28,57 +22,61 @@ Espectro::Espectro()
 	P_Speed;
 	V_radio;
 }
-void Espectro::move()
+string Espectro::move()
 {
-	if (posIndex == defaultRoute.size())
-	{
-		reverse(defaultRoute.begin(), defaultRoute.end());
-		posIndex = 0;
-	}
-	if (attackRoute.empty()) 
-	{
-		backtracking = true;
-		attacking = false;
-	}
-	if (backtrackRoute.empty()) 
-	{
-		backtracking = false;
-	}
-	if (!attacking)
-	{
-		if(backtracking)
+	if (!paralized) {
+		if (posIndex == defaultRoute.size())
 		{
-			currentPos = backtrackRoute.top();
-			backtrackRoute.pop();
+			reverse(defaultRoute.begin(), defaultRoute.end());
+			posIndex = 0;
 		}
-		else 
+		if (attackRoute.empty())
 		{
-			currentPos = defaultRoute[posIndex];
-			posIndex++;
+			backtracking = true;
+			attacking = false;
+		}
+		if (backtrackRoute.empty())
+		{
+			backtracking = false;
+			attacking = false;
+		}
+		if (!attacking)
+		{
+			if (backtracking)
+			{
+				currentPos = backtrackRoute.top();
+				backtrackRoute.pop();
+			}
+			else
+			{
+				currentPos = defaultRoute[posIndex];
+				posIndex++;
+			}
+		}
+		else
+		{
+			currentPos = attackRoute.front();
+			backtrackRoute.push(attackRoute.front());
+			attackRoute.pop();
 		}
 	}
-	else
-	{
-		currentPos = attackRoute.front();
-		backtrackRoute.push(attackRoute.front());
-		attackRoute.pop();
-	}
-	cout << "---" << "("<<get<0>(currentPos) <<","<< get<1>(currentPos)<<")";
+	//cout << "---" << "(" << get<0>(currentPos) << "," << get<1>(currentPos) << ")";
+	return to_string(get<0>(currentPos)) + ',' + to_string(get<1>(currentPos));
 
 }
-void Espectro::attack(int grid[25][25],tuple<int, int> playerPos)
+void Espectro::attack(int grid[25][25], tuple<int, int> playerPos)
 {
+	//setCurrentPos(pos);
 	attacking = true;
 	backtracking = false;
 	backtrackRoute.push(currentPos);
-	vector<tuple<int, int>> attack = getAttackRoute(grid, get<0>(currentPos), get<1>(currentPos), get<0>(playerPos), get<1>(playerPos));
-	for (int i = 0; i < attack.size(); i++)
+	attackRouteVec = getAttackRoute(grid, get<0>(currentPos), get<1>(currentPos), get<0>(playerPos), get<1>(playerPos));
+	for (int i = 0; i < attackRouteVec.size(); i++)
 	{
-		attackRoute.push(attack[i]);
+		attackRoute.push(attackRouteVec[i]);
 	}
-	
-
 }
+
 void Espectro::stop()
 {
 	attacking = false;
@@ -90,35 +88,72 @@ void Espectro::gotHurt()
 
 }
 
+void Espectro::paralizeStop()
+{
+	paralized = false;
+}
 void Espectro::paralize()
 {
-	
+	paralized = true;
 }
-void Espectro::defaultRouteVec() 
+void Espectro::setStats(float vel, int rango, float velAtaque)
 {
-	defaultRoute.push_back(make_tuple(12, 18));
-	defaultRoute.push_back(make_tuple(11, 18));
-	defaultRoute.push_back(make_tuple(10, 18));
-	defaultRoute.push_back(make_tuple(10, 17));
-	defaultRoute.push_back(make_tuple(10, 16));
-	defaultRoute.push_back(make_tuple(10, 15));
-	defaultRoute.push_back(make_tuple(10, 14));
-	defaultRoute.push_back(make_tuple(10, 13));
-	defaultRoute.push_back(make_tuple(11, 13));
-	defaultRoute.push_back(make_tuple(12, 13));
-	defaultRoute.push_back(make_tuple(13, 13));
-	defaultRoute.push_back(make_tuple(14, 13));
-	defaultRoute.push_back(make_tuple(15, 13));
-	defaultRoute.push_back(make_tuple(16, 13));
-	defaultRoute.push_back(make_tuple(17, 13));
+	string stats;
+	speed = vel;
+	range = rango;
+	chaseSpeed = velAtaque;
+	stats = to_string(speed) + ";" + to_string(range) + ";" + to_string(chaseSpeed);
+	//return stats;
 }
-void Espectro::printDefaultRoute()
-{
-	
-	for (int i = 0; i < defaultRoute.size(); i++)
-	{
 
-		cout << get<0>(defaultRoute[i]) << get<1>(defaultRoute[i]) << " ->";
-
-	}
-}
+//FUNCIONES DE OTRA LOGICA
+//void Espectro::defaultRouteVec(string defRoute)
+//{
+//	string temp=defRoute;
+//	while (!temp.empty()) 
+//	{
+//		defaultRoute.push_back(tupleFromString(temp.substr(0, temp.find(";"))));
+//		temp = temp.substr(temp.find(";")+1);
+//	}
+//}
+//tuple<int, int> Espectro::tupleFromString(string pos)
+//{
+//	int x = stoi(pos.substr(0, pos.find(',')));
+//	int y = stoi(pos.substr(pos.find(',') + 1));
+//	return make_tuple(x, y);
+//}
+//void Espectro::setCurrentPos(string pos) 
+//{
+//	int x = stoi(pos.substr(0,pos.find(',')));
+//	int y = stoi(pos.substr(pos.find(',') + 1));
+//	currentPos = make_tuple(x,y);
+//}
+//string Espectro::getDefaultRoute()
+//{
+//	string route = to_string(get<0>(defaultRoute[0]))+","+to_string(get<1>(defaultRoute[0])) + ";";
+//	for (int i = 1; i < defaultRoute.size(); i++)
+//	{
+//		
+//		route = route + to_string(get<0>(defaultRoute[i]))+"," + to_string(get<1>(defaultRoute[i]))+";";
+//		//cout << get<0>(defaultRoute[i]) << get<1>(defaultRoute[i]) << " ->";
+//
+//	}
+//	route.pop_back();
+//	
+//	return route;
+//}
+//string Espectro::getAttackRoute()
+//{
+//	
+//	string route = to_string(get<0>(attackRouteVec[0])) + "," + to_string(get<1>(attackRouteVec[0])) + ";";
+//	for (int i = 1; i < attackRouteVec.size(); i++)
+//	{
+//
+//		route = route + to_string(get<0>(attackRouteVec[i])) + "," + to_string(get<1>(attackRouteVec[i])) + ";";
+//		//cout << get<0>(attackRouteVec[i]) << get<1>(attackRouteVec[i]) << " ->";
+//
+//	}
+//	route.pop_back();
+//	attackString = route;
+//	return route;
+//}
--- Server/ServerVS/Espectro.h
+++ Server/ServerVS/Espectro.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <string>
 #include <vector>
 #include <tuple>
 #include <stack>
@@ -11,12 +12,9 @@ using namespace std;
 class Espectro
 {
 public:
-	int speed;
-	int range;
-	int life;
-	bool attacking;
-	bool backtracking;
-
+	//constructor
+	Espectro(string defRoute);
+	Espectro();
 	//fitness
 	int detections;
 	int hits;
@@ -27,24 +25,41 @@ public:
 	int R_Speed;
 	int P_Speed;
 	int V_radio;
+
 	
+	string move();
+	void attack(int grid[25][25], tuple<int, int>);
+	void gotHurt();
+	void stop();
+	void paralize();
+	void paralizeStop();
+	void setStats(float vel, int rango, float velAtaque);
+	vector<tuple<int, int>> getAttackRoute(int grid[25][25], int srcRow, int srcCol, int destRow, int destCol);
+
+	//tuple<int, int> playerPos;
+	//string nextPos;
+	//string attackString;
+	//tuple<int, int> tupleFromString(string pos);
+	//void defaultRouteVec(string defRoute);
+	//string getDefaultRoute();
+	//string getAttackRoute();
+	//void setCurrentPos(string pos);
+	
+private:
+	float speed;
+	int range;
+	float chaseSpeed;
+	int life;
+	bool attacking;
+	bool backtracking;
+	bool paralized;
 	int posIndex;
-	tuple<int, int> playerPos;
 	tuple<int, int> currentPos;
 	vector<tuple<int, int>> defaultRoute;
 	queue<tuple<int, int>> attackRoute;
 	stack<tuple<int, int>> backtrackRoute;
-	string nextPos;
+	vector<tuple<int, int>> attackRouteVec;
 
-	Espectro();
-	void move();
-	void attack(int grid[25][25],tuple<int, int>);
-	void gotHurt();
-	void stop();
-	void paralize();
-	vector<tuple<int, int>> getAttackRoute(int grid[25][25], int srcRow, int srcCol, int destRow, int destCol);
-	void defaultRouteVec();
-	void printDefaultRoute();
 };
 
 struct Population {
