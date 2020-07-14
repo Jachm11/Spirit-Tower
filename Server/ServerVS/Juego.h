93ac0809ea2f2945d57c85a4364770965dbbc2f8
Jose Alejandro Chavarria <joalchama@gmail.com>
2020-07-13 20:26:25 -0600

    docu juego y piso

--- Server/ServerVS/Juego.h
+++ Server/ServerVS/Juego.h
@@ -1,37 +1,97 @@
 #pragma once
 #include "piso.h"
 
-
+/// @brief Esta clase se asigna a cada cliente que entra al servidor, esta es un facade que se encarga de manejar todas las funciones necesarias de una partida, asi como almacenar sus datos
 class Juego
 {
 public:
 
 	Juego();
 	
+	//_____________________
 	//Funciones de partida
+
+	/// @brief Funcion que se encarga de instaciar el piso siguente
+	/// @param piso Numero de piso de 1 a 5
 	void generatePiso(int piso);
+
+	/// @brief Set puntos
+	/// @param puntos += puntos 
 	void pointsEarned(int puntos);
+
+	/// @brief Cofres++
 	void chestOpened();
+	
+	/// @brief Esta funcion se llama cuando el juego termina. Muestra el puntaje final, las muertes y los cofes abiertos en consola.
 	void endGame();
 
+	//_____________________
 	//Funciones de jugador
+
+	/// @brief Llama a la funcion que actualiza la posicion del jugador en el mapa
+	/// @param pos La posicion de la forma pos;x;y
 	void setPlayerPos(string pos);
+
+	/// @brief Ejecuta el ciclo necesario que actualiza las vidas del jugador
+	/// @return Vidas totales del jugador
 	int playerHealed();
+
+	/// @brief Ejecuta el ciclo necesario cuando el jugador ha sido golpeado
+	/// @return Vidas totales del jugador
 	int playerHit();
+
+	/// @brief Ejecuta el ciclo necesario cuando el jugador muere 
 	void playerDied();
+
+	/// @brief Ejecuta el ciclo necesario cuando el jugador sale de una zona segura
 	void playerVisible();
+
+	/// @brief Ejecuta el ciclo necesario cuando el jugador entra en una zona segura
 	void playerInvisible();
+
+	/// @brief Pregunta por el estado del jugador
+	/// @return El estado del jugador 1 si invisible, 0 si visible
 	string playerState();
 
-
+	//_____________________
 	//Funciones del enemigo
+
+	/// @brief Pregunta por la ruta de patrullaje de un espectro especifico del piso
+	/// @param ID Id del espectro 0, 1, 2
+	/// @return Ruta default 
 	string defaultResponse(int ID);
+
+	/// @brief Pregunta por los stats que tiene el espectro del piso
+	/// @param ID Id del espectro 0, 1, 2
+	/// @return Los stats del espectro
 	string statsResponse(int ID);
+
+	/// @brief Pregunta por la ruta de ataque, esa ruta se calcula desde la posicion del espectro a la del jugador con A*
+	/// @param ID Id del espectro 0, 1, 2
+	/// @param pos Posicion del espectro
+	/// @return  La ruta de ataque hasta el jugador, si no hay retorna "NO"
 	string attackResponse(int ID, string pos);
+
+	/// @brief Pregunta por la ruta de backtrack de un espectro del piso
+	/// @param ID Id del espectro 0, 1, 2
+	/// @return La ruta de backtrack para el espectro
 	string backtrackResponse(int ID);
+
+	/// @brief Set para una nueva entrada en la ruta de backtrack
+	/// @param ID Id del espectro 0, 1, 2
+	/// @param pos Posicion a agregar
 	void backtrackPos(int ID, string pos);
+
+	/// @brief Ejecuta el ciclo necesario cuando un espectro del piso es derrotado
+	/// @param ID Id del espectro 0, 1, 2
 	void enemyDied(int ID);
+
+	/// @brief Ejecuta el ciclo necesario cuando un espectro del piso detecta al jugador
+	/// @param ID Id del espectro 0, 1, 2
 	void enemyDetected(int ID);
+
+	/// @brief Ejecuta el ciclo necesario cuando un espectro del piso asesina al jugador
+	/// @param ID Id del espectro 0, 1, 2
 	void enemyKilled(int ID);
 
 
@@ -147,10 +207,13 @@ private:
 
 	int Matriz4[25][25];
 
-	int Matriz5[25][25];
 
+	/// @brief Set de puntajes maximos
+	/// @param arr Array donde se guardan los puntos maximos del cada nivel 
 	void setPuntajes(int arr[5]);
 
+
+	/// @brief Set de las rutas de cada espectro
 	void setRutas();
 
 };
--- Server/ServerVS/Piso.h
+++ Server/ServerVS/Piso.h
@@ -16,28 +16,69 @@ public:
 	//Constructores
 
 	Piso();
+
+	/// @brief Contructor del piso 1 
+	/// @param E1 Ruta default del espectro 0
+	/// @param E2 Ruta default del espectro 1
+	/// @param E3 Ruta default del espectro 2
+	/// @param puntajeMax Puntaje maximo del nivel
+	/// @return Instancia de piso 1
 	Piso(string E1, string E2, string E3, int puntajeMax);
+
+	/// @brief Contructor del pisos 2, 3, 4 
+	/// @param E1 Ruta default del espectro 0
+	/// @param E2 Ruta default del espectro 1
+	/// @param E3 Ruta default del espectro 2
+	/// @param puntajePasado Puntaje que gano el jugador en el piso anterior
+	/// @param MuertesPasadas Puntaje que gano el jugador en el piso anterior 
+	/// @param j puntero al jugador en juego
+	/// @param pastGen Espectros del piso anterior
+	/// @param puntajeMax Puntaje maximo del nivel
+	/// @return Instacia de piso 2, 3, o 4 
 	Piso(string E1, string E2, string E3, int puntajePasado, int MuertesPasadas, Jugador j, std::vector <Espectro> pastGen, int puntajeMax);
+
+	/// @brief Constructor para piso final
+	/// @param boss Valor vacio
+	/// @return Instancia Piso 5
 	Piso(bool boss);
 
 	//Puntos
-
+	
+	/// @brief Actualiza los puntos de ganados piso
+	/// @param puntos cantidad de puntos ganados
 	void updatePuntaje(int puntos);
 
 	//Jugador
 
+	/// @brief Actualiza las muertes del jugador este piso
 	void jugadorMuere();
 
 	//Espectros
-
+	
+	/// @brief Actualiza las muertes del espectro del piso
+	/// @param ID Id del espectro del piso
 	void EspectroMurio(int ID);
+
+	/// @brief Actualiza las deteciones del espectro del piso
+	/// @param ID Id del espectro del piso
 	void EspectroVio(int ID);
+
+	/// @brief Actualiza los asesinatos del espectro del piso
+	/// @param ID Id del espectro del piso
 	void EspectroMato(int ID);
 
 private:
 
+	/// @brief Genera un stat aleatorio para los espectros del primer piso
+	/// @return stat aleatorio para un espectro
 	float getRandomStat();
 
+	/// @brief Llama a la funcion que ejecuta un ciclo del algoritmo genetico
+	/// @param pastGen Generacion de espectros pasada
+	/// @param puntajePasado Puntaje obtenido en el piso anterior
+	/// @param muertesPasadas Muertes en el piso anterior
+	/// @param j La instancia dejugador
+	/// @return Retorna un vector con los nuevos stats para un espectro
 	vector<float> getGeneticStat(std::vector <Espectro> pastGen, int puntajePasado, int muertesPasadas, Jugador j);
 
 };
